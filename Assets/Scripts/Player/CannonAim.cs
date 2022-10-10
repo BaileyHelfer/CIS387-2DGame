07651312f4a730ae2e535ae6b2fd81211fa5b218
samcam004 <samcam@umich.edu>
2022-10-10 14:18:46 -0400

    Cannon with movement
    Added a cannon sprite attached to the player by a hinge joint. The cannon direction changes with mouse position. When Fire1 (left click) is pressed the character moves in the opposite direction.

---
+++ Assets/Scripts/Player/CannonAim.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonAim : MonoBehaviour
+{
+    public float shootingPower = 1000f;
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 mousePos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rb.AddForce(-transform.up * shootingPower);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
