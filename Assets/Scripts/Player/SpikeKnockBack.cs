67734a2d7a838e7f0aef0ac503c86d468c83110c
lap-tops <114961694+lap-tops@users.noreply.github.com>
2022-10-12 17:03:19 -0400

    knockback

--- Assets/Scripts/Player/SpikeKnockBack.cs
+++ Assets/Scripts/Player/SpikeKnockBack.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spikes : MonoBehaviour
-{  
-    int coinLoss = -3;
+public class SpikeKnockBack : MonoBehaviour
+{
     public Rigidbody2D rb;
+    public float knockBack = 5f;
+    
+
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Trap"))
         {
-           
-    ScoreManager.instance.ChangeScore(coinLoss);
+
+            rb.velocity = new Vector3(0, 10, 0);
         }
     }
+    }
     
-    
-}
