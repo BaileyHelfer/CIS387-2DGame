33047db3697246cd76b3e485ab0d7646beb0a41e
samcam004 <samcam@umich.edu>
2022-11-02 00:34:42 -0400

    Revert "Update MainMenu.cs"
    This reverts commit d4cda37e1b5517d50a70939460a0294aa097c5ba.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,36 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    bool LoadingInitiated = false;
-    int x;
-
-    public void PlayGame()
-    {
-        if (!LoadingInitiated)
-        {
-            x = 0;
-            StartCoroutine(DelayedLoad(x));
-            LoadingInitiated = true;
-        }
-    }
-
-    public void QuitGame()
-    {
-            x = 1;
-            StartCoroutine(DelayedLoad(x));
-            LoadingInitiated = true;
-    }
-
-    IEnumerator DelayedLoad(int y)
+    void Start()
     {
         GetComponent<AudioSource>().Play();
+    }
 
-        yield return new WaitForSeconds(1);
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 
-        if (x == 0)
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        else
-            Application.Quit();
-
+    public void ExitGame ()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
